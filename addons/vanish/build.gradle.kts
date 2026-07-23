0ae081acb5173d24a86418f147d752c41fca0959
mercurialmusic <21208123+mercurialmusic@users.noreply.github.com>
2026-07-23 02:22:15 -0400

    Update SuperVanish dependency to version 6.3.0

--- addons/vanish/build.gradle.kts
+++ addons/vanish/build.gradle.kts
@@ -1,7 +1,7 @@
 description = "Integration with various vanish plugins for squaremap"
 
 dependencies {
-    compileOnly("com.github.LeonMangler:SuperVanish:6.2.19") {
+    compileOnly("com.github.LeonMangler:SuperVanish:6.3.0") {
         isTransitive = false
     }
 }
